e7d76a030d4de74d43cbd64cf76b5c4750df980b
sbreddy17 <bhuvaneswarr412@gmail.com>
2024-11-11 16:34:06 +0530

    Update BillingController.java

--- src/main/java/com/drucare/BillingController.java
+++ src/main/java/com/drucare/BillingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BillingController {
 	@GetMapping("/")
 	public String billing() {
-		return "Welcome to billing version One";
+		return "Welcome to billing version two";
 	}
 
 }
